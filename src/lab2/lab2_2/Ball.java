7d1e644be7fa5fd2cd5c1890d72002907524fead
sxciummm <gleb_poyakov_06@mail.ru>
2025-09-06 21:48:41 +0300

    add lab2_1,2,3

---
+++ src/lab2/lab2_2/Ball.java
@@ -0,0 +1,37 @@
+package lab2.lab2_2;
+
+public class Ball {
+    private double x = 0.0;
+    private double y = 0.0;
+
+    public Ball(double x , double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Ball() {
+        this(0.0,0.0);
+    }
+
+    public double getX() {return x;}
+    public double getY() {return y;}
+
+    public void setX(double x) {this.x = x;}
+    public void setY(double y) {this.y = y;}
+
+    public void setXY(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(double xDisp, double yDisp) {
+        this.x += xDisp;
+        this.y += yDisp;
+    }
+
+    @Override
+    public String toString() {
+        return "x = " + x + "\n" + "y = " + y + "\n";
+    }
+
+}
---
+++ src/lab2/lab2_3/Circle.java
@@ -0,0 +1,32 @@
+package lab2.lab2_3;
+
+public class Circle {
+    private Point center;
+    private double r;
+
+    public Circle() {
+        this(new Point(),1.0);
+    }
+
+    public Circle(Point center, double r) {
+        this.center = center;
+        this.r = r;
+    }
+
+    public Circle(double x, double y , double r) {
+        this.center = new Point(x , y);
+        this.r = r;
+    }
+
+    public Point getCenter() {return center;}
+    public void setCenter(Point center) {this.center = center;}
+
+    public double getR() {return r;}
+    public void setR(double r) {this.r = r;}
+
+    @Override
+    public String toString(){
+        return "Circle: " + "\n" + "center = " + center + "\n" + "radius = "+ r;
+    }
+
+}
