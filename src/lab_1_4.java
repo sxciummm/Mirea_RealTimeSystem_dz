79dd8e99a9ecaac1bd57e7ea419eaa4bd977b682
sxciummm <gleb_poyakov_06@mail.ru>
2025-09-06 17:13:14 +0300

    Initial commit

---
+++ src/lab_1_4.java
@@ -0,0 +1,11 @@
+public class lab_1_4 {
+    public static void main(String[] args) {
+        System.out.println("Первые 10 чисел гармонического ряда:");
+
+        double harm = 0.0;
+        for (int n = 1; n<=10; n++){
+            harm += 1.0/n;
+            System.out.printf("H(%d) = %.2f%n", n , harm);
+        }
+    }
+}
---
+++ src/lab_1_5.java
@@ -0,0 +1,18 @@
+public class lab_1_5 {
+    public static long factorial(int a) {
+
+        if (a<0) {
+            System.out.print("Значение не может быть меньше 0!");
+        }
+
+        long fact = 1;
+        for (int i = 1; i <= a;i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("Факториал: " + factorial(10));
+    }
+}
